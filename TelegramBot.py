800aef73cbbd32eaf63db40ee489c88790cd85c3
Janik Hasenkamp <Jhasi1604@gmail.com>
2020-08-07 13:37:28 +0200

    Co-authored-by: deutschj@dhbw-loerrach.de <deutschj@dhbw-loerrach.de> Co-authored-by: Mash-Fr <marcschnekenburger@gmail.com> Co-authored-by: niklas@fam-zi.de <niklas@fam-zi.de>

--- TelegramBot.py
+++ TelegramBot.py
@@ -6,7 +6,7 @@ import re
 from flashtext import KeywordProcessor
 import time
 from telegram.ext import MessageHandler, Filters
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, ReplyKeyboardRemove
 updater = Updater(
     token='1298615678:AAGUe-rqG2A8tQnlTPwQB6GUFZfqKIze1Kk', use_context=True)
 
@@ -61,28 +61,43 @@ def check_user_existing(update):
     return False
 
 
+def update_time(update):
+    userId = update.effective_chat.id
+
+
 def start(update, context):
     userId = update.effective_chat.id
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Um die Sprache zu ändern tippen Sie /language")
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="Um die Sprache zu ändern tippen Sie /language")
     if check_user_existing(update):
         context.bot.send_message(chat_id=update.effective_chat.id,
                                  text="Willkommen zurück, " + str(update.message.chat.first_name))
     else:
-        cursor.execute("INSERT INTO Users(UserId, LanguageSet) VALUES (?,?);", userId, "GPM_Prince_DE")
+        cursor.execute(
+            "INSERT INTO Users(UserId, LanguageSet) VALUES (?,?);", userId, "GPM_Prince_DE")
         conn.commit()
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Hallo, ich bin der PMBot.\nBitte geben Sie zunächst Ihre Abteilung an:")
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text="Hallo, ich bin der PMBot.\nBitte geben Sie zunächst Ihre Abteilung an:")
         AskDepartment(update, context)
 
 
-def select_base(userId, stripped_text):
-    cursor.execute("SELECT LanguageSet FROM Users WHERE UserId = ?", userId)
-    language = cursor.fetchone().LanguageSet
+def quiz(update, context):
+    markup = ReplyKeyboardMarkup(
+        keyboard=[['1', '2']], one_time_keyboard=True, resize_keyboard=True)
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="/1 Test 1 \n /2 Test 2", reply_markup=markup)
+
+
+def select_base(userId, stripped_text, language):
+
     print(language)
     if "gpm" in stripped_text:
-        cursor.execute("SELECT Titel FROM " + language + " WHERE Datenbasis= ?", "gpm")
+        cursor.execute("SELECT Titel FROM " + language +
+                       " WHERE Datenbasis= ?", "gpm")
         database = "gpm"
     else:
-        cursor.execute("SELECT Titel FROM " + language + " WHERE Datenbasis = ?;", "prince2")
+        cursor.execute("SELECT Titel FROM " + language +
+                       " WHERE Datenbasis = ?;", "prince2")
         database = "prince2"
     return database
 
@@ -95,14 +110,16 @@ def compare(input_string, keywords):
 
 def echo(update, context):
     userId = update.effective_chat.id
+    cursor.execute("SELECT LanguageSet FROM Users WHERE UserId = ?", userId)
+    language = cursor.fetchone().LanguageSet
     stripped_text = re.sub('[^A-Za-z0-9 ]+', '', update.message.text)
     # Endungen bei input beachten
     # Umlaute, Satzzeichen,
-    database = select_base(userId, stripped_text)
+    database = select_base(userId, stripped_text, language)
     keywords = cursor.fetchall()
     keywords = [x.Titel for x in keywords]
     responses = compare(stripped_text, keywords)
-    
+
     if len(responses) > 0:
         for keyword in responses:
             print(keyword)
@@ -116,7 +133,8 @@ def echo(update, context):
     else:
         response = database.capitalize() + " Schlüsselwörter: \n"
         response += ";  ".join([k.capitalize() for k in keywords])
-        context.bot.send_message(chat_id=update.effective_chat.id, text=response)
+        context.bot.send_message(
+            chat_id=update.effective_chat.id, text=response)
 
 
 def button(update, context):
@@ -125,18 +143,20 @@ def button(update, context):
     userId = update.effective_chat.id
 
     Data = query.data.split(";")
-   
+
     cursor.execute("UPDATE Users SET " +
                    str(Data[0]) + " = ? WHERE UserId = ?", Data[1], userId)
     conn.commit()
-    query.edit_message_text(text="Danke für die Eingabe")
+    query.edit_message_text(text="Eingabe gespeichert")
 
 
 start_handler = CommandHandler('start', start)
 language_handler = CommandHandler('language', ChangeLanguage)
+quiz_handler = CommandHandler('quiz', quiz)
 dispatcher.add_handler(CallbackQueryHandler(button))
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(language_handler)
+dispatcher.add_handler(quiz_handler)
 
 echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
 dispatcher.add_handler(echo_handler)
