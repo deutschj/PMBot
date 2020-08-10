582ca53fea2bd33ff04d20452ef03a17224c97dc
TheNymon <TheNymon@users.noreply.github.com>
2020-08-10 14:25:52 +0200

    Co-authored-by: Mash-Fr <marcschnekenburger@gmail.com> Co-authored-by: hasenkaj@dhbw-Loerrach.de <hasenkaj@dhbw-Loerrach.de> Co-authored-by: deutschj@dhbw-loerrach.de <deutschj@dhbw-loerrach.de>

--- TelegramBot.py
+++ TelegramBot.py
@@ -7,7 +7,7 @@ import random
 from datetime import datetime, date
 import re
 import time
-import threading
+import _thread
 from flashtext import KeywordProcessor
 from telegram.ext import MessageHandler, Filters
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, ReplyKeyboardRemove
@@ -29,17 +29,17 @@ def ChangeLanguage(update, context):
     global bot
     bot = context.bot
     userId = update.effective_chat.id
-    # context.bot.send_message(chat_id=update.effective_chat.id, text="Hello, " + str(update.message.chat.first_name) + " I'm a bot")
     # Tastatur soll die Namen der Sprachentabelle zurückgeben
     keyboard = [[InlineKeyboardButton("Deutsch", callback_data="LanguageSet;GPM_Prince_DE"),
                  InlineKeyboardButton("English", callback_data="LanguageSet;GPM_Prince_EN")]]
     reply_markup = InlineKeyboardMarkup(keyboard)
     update.message.reply_text(
-        'Bitte wählen Sie Ihre Sprache: ', reply_markup=reply_markup)
+        "Bitte wählen Sie Ihre Sprache / Please select your language: ", reply_markup=reply_markup)
 
-    # cursor.execute("UPDATE Users SET LanguageSet = ? WHERE UserId = ?", , userId)
-
-
+def get_user_language(userId):
+    cursor.execute("SELECT LanguageSet FROM Users WHERE UserId = ?", userId)
+    return cursor.fetchone().LanguageSet.strip().strip(",")
+    
 def check_user_offline():
     while True:
         
@@ -50,35 +50,43 @@ def check_user_offline():
 
         for last_message in last_messages:
             time_dif = now - last_message[1]
-            if time_dif >= 12:  # time in seconds (15 days = 1296000 sec)
-                bot.send_message(
-                    chat_id=last_message[0], text="Eyy du, schreib mal wieder!")
-
-        time.sleep(10)
+            if time_dif >= 1296000:  # time in seconds (15 days = 1296000 sec)
+                if "GPM_Prince_DE" in get_user_language(last_message[0]):
+                    response ="Es sind mindestens 15 Tage seit der letzten Interaktion vergangen.\nMit /quiz kann ein kurzes Quiz gestartet werden."
+                else:
+                    response = "It has been 15 days since your last interaction.\nYou can start a short quiz with /quiz." 
+                bot.send_message(chat_id=last_message[0], text = response)
+        time.sleep(86400)
 
 
 if __name__ == "__main__":
-    t1 = threading.Thread(name="check_user_offline",
-                          target=check_user_offline())
-    t1.start()
+    _thread.start_new_thread(check_user_offline) # separate thread for counting the time since last Bot usage
 
 
 def AskDepartment(update, context):
     userId = update.effective_chat.id
     keyboard = []
-
-    cursor.execute("SELECT DepartmentDe FROM Department")
+    language = get_user_language(userId)
+    if "GPM_Prince_DE" in language:
+        cursor.execute("SELECT DepartmentDe FROM Department")
+        dp = [departments.DepartmentDe for departments in cursor.fetchall()]
+    else:
+        cursor.execute("SELECT DepartmentEn FROM Department")
+        dp = [departments.DepartmentEn for departments in cursor.fetchall()]
     temp = []
-    for counter, departments in enumerate(cursor.fetchall()):
-        dp = departments.DepartmentDe
-        temp.append(InlineKeyboardButton(dp.strip().strip(","),
-                                         callback_data="Department;" + dp.strip().strip(",")))
+    for counter, departments in enumerate(dp):
+        temp.append(InlineKeyboardButton(departments.strip().strip(","),
+                                         callback_data="Department;" + departments.strip().strip(",")))
         if counter % 2 == 1:
             keyboard.append(temp)
 
     reply_markup = InlineKeyboardMarkup(keyboard)
+    if "GPM_Prince_DE" in language:
+        response = "Bitte wählen Sie Ihre Abteilung: "
+    else:
+        response = "Please select your department: "
     update.message.reply_text(
-        'Bitte wählen Sie Ihre Abteilung: ', reply_markup=reply_markup)
+        response, reply_markup=reply_markup)
 
 
 def check_user_existing(update):
@@ -102,24 +110,35 @@ def update_time(update):
 def start(update, context):
     bot = context.bot
     userId = update.effective_chat.id
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="Um die Sprache zu ändern tippen Sie /language")
+    language = get_user_language(userId)
     if check_user_existing(update):
+        if "GPM_Prince_DE" in language:
+            response = "Willkommen zurück, "
+        else:
+            response = "Welcome back, "
         context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text="Willkommen zurück, " + str(update.message.chat.first_name))
+                                 text= response + str(update.message.chat.first_name))
     else:
         cursor.execute(
             "INSERT INTO Users(UserId, LanguageSet) VALUES (?,?);", userId, "GPM_Prince_DE")
         conn.commit()
-        context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text="Hallo, ich bin der PMBot.\nBitte geben Sie zunächst Ihre Abteilung an:")
+        if "GPM_Prince_DE" in language:
+            response = "Hallo, ich bin der PMBot.\nBitte geben Sie zunächst Ihre Abteilung an:"
+        else:
+            response = "Hello, I am the PMBot.\nPlease select your department first:"
+        context.bot.send_message(chat_id=update.effective_chat.id, text= response)
         AskDepartment(update, context)
+    if "GPM_Prince_DE" in language:
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="Um die Sprache zu ändern tippen Sie /language.\nUm die Abteilung zu wechseln tippen Sie /department")
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="To change your language, please type /language.\nTo change your department, please type /department.\nTo get an overview over all commands type /help")
 
 
 def quiz(update, context):
     userId = update.effective_chat.id
-    cursor.execute("SELECT LanguageSet FROM Users WHERE UserId = ?", userId)
-    language = cursor.fetchone().LanguageSet
+    language = get_user_language(userId)
     cursor.execute("SELECT Titel FROM " + language)
     temp = []
     keyboard = []
@@ -129,6 +148,8 @@ def quiz(update, context):
         dp = [x.Titel for x in cursor.fetchall()]
 
     random.shuffle(dp)
+    dp = set(dp)
+    dp = list(dp)
     dp = dp[:5]
     right_answer = random.randrange(0, 4)
     cursor.execute("SELECT Definition FROM " + language +
@@ -151,8 +172,11 @@ def quiz(update, context):
             temp = []
 
     reply_markup = InlineKeyboardMarkup(keyboard)
-    question = "Welches Keyword passt zur Definition der Datenbasis " + \
-        database.upper() + "?:\n\n" + question
+    if "GPM_Prince_DE" in language:
+            response = "Welches Keyword passt zur Definition der Datenbasis "
+    else:
+        response = "Which keyword matches the definition from the database "
+    question = response + database.upper() + "?\n\n" + question
     update.message.reply_text(
         question, reply_markup=reply_markup)
 
@@ -201,7 +225,7 @@ def echo(update, context):
             context.bot.send_message(
                 chat_id=update.effective_chat.id, text=response)
     else:
-        response = database.capitalize() + " Schlüsselwörter: \n"
+        response = database.capitalize() + " Schlüsselwörter / keywords: \n"
         response += ";  ".join([k.capitalize() for k in keywords])
         context.bot.send_message(
             chat_id=update.effective_chat.id, text=response)
@@ -211,34 +235,40 @@ def button(update, context):
     query = update.callback_query
     query.answer()
     userId = update.effective_chat.id
-
+    language = get_user_language(userId)
     Data = query.data.split(";")
 
     if "answer" in Data[0]:
         if Data[0] == "right_answer":
-            query.edit_message_reply_markup(inline_message_id=query.inline_message_id,
-                                            reply_markup=None)
-            context.bot.send_message(
-                chat_id=update.effective_chat.id, text="Richtige Antwort")
+            query.edit_message_reply_markup(inline_message_id=query.inline_message_id, reply_markup=None)
+            if "GPM_Prince_DE" in language:
+                response = "Richtige Antwort!"
+            else:
+                response = "That's correct!"
+            context.bot.send_message(chat_id=update.effective_chat.id, text= response)
         else:
-            query.edit_message_reply_markup(inline_message_id=query.inline_message_id,
-                                            reply_markup=None)
-            context.bot.send_message(
-                chat_id=update.effective_chat.id, text="Falsche Antwort. Du Idiot. Richtig wäre " + Data[1] + " gewesen.")
+            query.edit_message_reply_markup(inline_message_id=query.inline_message_id, reply_markup=None)
+            if "GPM_Prince_DE" in language:
+                response = "Falsche Antwort. Richtig wäre " + Data[1] + " gewesen."
+            else:
+                response = "Wrong answer. " + Data[1] + "would have been correct."
+            context.bot.send_message(chat_id=update.effective_chat.id, text= response)
     else:
         cursor.execute("UPDATE Users SET " +
                        str(Data[0]) + " = ? WHERE UserId = ?", Data[1], userId)
         conn.commit()
-        query.edit_message_text(text="Eingabe gespeichert")
+        query.edit_message_text(text="✅")
 
 
 start_handler = CommandHandler('start', start)
-language_handler = CommandHandler('language', ChangeLanguage)
+language_handler = CommandHandler('language', ChangeLanguage) 
+department_handler = CommandHandler('department', AskDepartment)
 quiz_handler = CommandHandler('quiz', quiz)
 dispatcher.add_handler(CallbackQueryHandler(button))
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(language_handler)
 dispatcher.add_handler(quiz_handler)
+dispatcher.add_handler(department_handler)
 
 echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
 dispatcher.add_handler(echo_handler)
